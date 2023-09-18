12fdc43289c440f837f4043d658ba31d10bf0fde
lacrisun <gustipanji2006@gmail.com>
2023-09-18 20:37:00 +0700

    fix img

--- components/hero2.js
+++ components/hero2.js
@@ -6,7 +6,7 @@ export default function HeroDua() {
     return (
         <div id="aboutsection" className="hero min-h-screen bg-secondary">
             <div className="hero-content text-slate-800 flex-col lg:flex-row-reverse">
-                <img src="/staffalharom.jpg" className="max-w-sm rounded-lg shadow-2xl my-5" />
+                <img src="/staffalharom.jpg" className="max-w-fit rounded-lg shadow-2xl my-5" />
                     <div>
                     <h1 className="text-5xl font-bold">Tentang Kami</h1>
                     <p className="py-6">Kami, PT INDO MITRA ALHAROM, dengan bangga menghadirkan diri sebagai penyelenggara jasa Umrah & Haji terpercaya. Dedikasi kami tak tergoyahkan dalam memberikan fasilitas pelayanan yang sepenuhnya difokuskan pada kepuasan dan kenyamanan jamaah kami.</p>
