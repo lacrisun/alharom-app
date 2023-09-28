ff587e9494c0edb9c49d782c7dbd35483488ffe9
lacrisun <gustipanji2006@gmail.com>
2023-09-28 15:57:04 +0700

    fix hero in homepage

--- components/hero2.js
+++ components/hero2.js
@@ -5,12 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export default function HeroDua() {
     return (
         <div id="aboutsection" className="hero min-h-screen bg-secondary">
-            <div className="hero-content text-slate-800 flex-col lg:flex-row-reverse">
-                <img src="/staffalharom.jpg" className="max-w-xs rounded-lg shadow-2xl my-5" />
-                    <div>
-                    <h1 className="text-5xl font-bold">Tentang Kami</h1>
-                    <p className="py-6">Kami, PT INDO MITRA ALHAROM, dengan bangga menghadirkan diri sebagai penyelenggara jasa Umrah & Haji terpercaya. Dedikasi kami tak tergoyahkan dalam memberikan fasilitas pelayanan yang sepenuhnya difokuskan pada kepuasan dan kenyamanan jamaah kami.</p>
+            <div className="w-full p-10 text-slate-800 flex-col text-center lg:flex-row-reverse">
+                <div className="flex justify-center">
+                    <img src="/staffalharom.jpg" className="w-full md:max-w-xl rounded-lg shadow-2xl my-5 " />
                 </div>
+                    <div>
+                        <h1 className="text-5xl font-bold">Tentang Kami</h1>
+                        <p className="text-xl py-6">Kami, PT INDO MITRA ALHAROM, dengan bangga dan penuh semangat, senantiasa berkomitmen tinggi sebagai penyelenggara jasa Umrah & Haji terpercaya yang dikenal atas dedikasi tak tergoyahkan kami. Selama perjalanan panjang kami, kami telah meneguhkan tekad kami untuk menyediakan fasilitas pelayanan yang sepenuhnya berorientasi pada kepuasan dan kenyamanan jamaah kami, sehingga menjadikan pengalaman ibadah mereka tak terlupakan.</p>
+                    </div>
             </div>
         </div>
     )
