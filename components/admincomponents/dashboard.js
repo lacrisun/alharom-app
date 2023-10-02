843d900128b2caf5f464cda45ed20d3c5b6f2f24
lacrisun <gustipanji2006@gmail.com>
2023-10-02 21:01:13 +0700

    added cards

--- components/admincomponents/dashboard.js
+++ components/admincomponents/dashboard.js
@@ -1,9 +1,21 @@
-
+import prisma from "@/lib/prisma"
 
 export default function Dashboard() {
+
+    const getData = async (e) => {
+        const res = await prisma.user.count()
+        return res
+    }
+    
     return (
-        <div className="grid bg-primary min-h-screen z-40">
-        
+        <div className="grid min-h-screen z-40 bg-red-950">
+            <div className="stats h-min w-min gap-4 m-4 shadow">
+                <div className="stat bg-primary">
+                    <div className="stat-title">Total Jumlah Pengguna</div>
+                    <div className="stat-value">{getData()}</div>
+                    <div className="stat-desc">Bjir momen</div>
+                </div>
+            </div>   
         </div>
     )
 }
