ea0e829e75ecc517fe4014e5a36f3c8f621bde1c
lacrisun <gustipanji2006@gmail.com>
2023-10-14 17:44:55 +0700

    wbehok

--- app/api/webhooks/xendit/route.js
+++ app/api/webhooks/xendit/route.js
@@ -1,51 +1,29 @@
 
-export async function POST(req) {
-    const secret = process.env.XENDIT_WEBHOOK_SECRET
+import { NextRequest, NextResponse } from 'next/server'
 
-    const xenditCallbackToken = 'Z9A3UpJEokuMaDXTBxF2CCv8oJcXmfN2pxVmhaQRuQqCvg9t'
-  
-    // Get the request headers
-    const reqHeaders = req.headers;
-    const xIncomingCallbackTokenHeader = reqHeaders['x-callback-token'] || '';
-  
-    // Verify if the request is from Xendit
-    if (xIncomingCallbackTokenHeader === xenditCallbackToken) {
-      // Request is verified to be from Xendit
-  
-      // Read the raw request input as JSON
-      const rawRequestInput = await new Promise((resolve, reject) => {
-        let body = '';
-        req.on('data', (chunk) => {
-          body += chunk;
-        });
-        req.on('end', () => {
-          resolve(body);
-        });
-      });
-  
-      // Parse the raw input into an associative array
-      const arrRequestInput = JSON.parse(rawRequestInput);
-      
-      console.log(arrRequestInput);
-  
-      const {
-        id,
-        external_id,
-        user_id,
-        status,
-        paid_amount,
-        paid_at,
-        payment_channel,
-        payment_destination,
-      } = arrRequestInput;
-  
-      // You can now use the above object properties for further processing in your application.
-  
-      // Send a response (modify this as needed)
-      return new Response('Callback received and processed successfully.', { status: 200 });
-    } else {
-      // Request is not from Xendit, reject with HTTP status 403
-      return new Response('Forbidden', { status: 403 });
-    }
+export async function POST(request) {
+  const xenditXCallbackToken = 'Z9A3UpJEokuMaDXTBxF2CCv8oJcXmfN2pxVmhaQRuQqCvg9t';
+
+  const xIncomingCallbackTokenHeader = request.headers.get('x-callback-token');
+
+  if(xIncomingCallbackTokenHeader === xenditXCallbackToken){
+    const arrRequestInput = JSON.parse(await request.text());
+
+    const {
+      id: _id,
+      external_id: _externalId,
+      user_id: _userId,
+      status: _status,
+      paid_amount: _paidAmount,
+      paid_at: _paidAt,
+      payment_channel: _paymentChannel,
+      payment_destination: _paymentDestination
+    } = arrRequestInput;
+
+    // Do something with the data here
+
+    return NextResponse.json({ message: 'Webhook processed successfully' });
+  } else {
+    return NextResponse.json({ message: 'Invalid token' }, { status: 403 });
   }
-  
+}
