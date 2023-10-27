28badf06b315bdf7e095d59f2258a0435227cec4
lacrisun <gustipanji2006@gmail.com>
2023-10-27 20:15:42 +0700

    no

--- app/pendaftaran/page.js
+++ app/pendaftaran/page.js
@@ -21,13 +21,9 @@ export default function Pendaftaran() {
     
     const xenditInvoiceClient = new InvoiceClient({secretKey: secret})
 
-    const {data: session, status} = useSession()
+    const {data: session} = useSession()
 
-    if (status === "loading") {
-        return (
-            <h1>Loading...</h1>
-        )
-    }
+    
 
     const [paketumrah, setPaketumrah] = useState("Umrah Reguler (Silver)");
     const [tipekamar, setTipekamar] = useState("Quad");
