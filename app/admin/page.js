c9e4c576e19fdc80f6f64574ddbeb5deed15055e
lacrisun <gustipanji2006@gmail.com>
2023-11-05 21:23:48 +0700

    admin page rendering alpha

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,13 +1,38 @@
+'use client'
+
 import AdminNav from "@/components/admincomponents/adminnavbar";
 import Dashboard from "@/components/admincomponents/dashboard";
+import LoadingPage from "@/components/loading";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Admin() {
+    const router = useRouter()
+    const {data: session, status} = useSession()
+
+    const adminRole = session?.user.isadmin
+
+    useEffect(() => {
+        if (!adminRole) {
+            return (
+                router.push('/')
+            )
+
+        }
+    }, [adminRole])
+
+    if (status === 'loading') {
+        return (
+            <LoadingPage />
+        )
+    }
     return (
         <>
         
-        <AdminNav/>
-
+        <h1>tes</h1>
 
         </>
     )
+
 }
