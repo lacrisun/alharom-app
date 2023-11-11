949ea0db3dcdd8e4cbf65bbd88774505692dd6ec
lacrisun <gustipanji2006@gmail.com>
2023-11-11 12:33:12 +0700

    added route segment config

--- app/admin/page.js
+++ app/admin/page.js
@@ -29,7 +29,7 @@ export default function Admin() {
 
     const fetchUserCount = async () => {
         try {
-            const response = await fetch('/api/usercount', {cache: 'no-store', next: { revalidate: 0 }});
+            const response = await fetch('/api/usercount');
             const data = await response.json();
             setUserCount(data.responsedata)
         } catch (error) {
@@ -39,7 +39,7 @@ export default function Admin() {
 
     const fetchCurrentMonthUserCount = async () => {
         try {
-            const response = await fetch('/api/userpermonth', {cache: 'no-store', next: { revalidate: 0 }});
+            const response = await fetch('/api/userpermonth');
             const data = await response.json();
             setUserpermonth(data.responsedata)
         } catch (error) {
@@ -49,7 +49,7 @@ export default function Admin() {
 
     const fetchUsers = async () => {
         try {
-            const response = await fetch('/api/userlist', {cache: 'no-store', next: { revalidate: 0 }})
+            const response = await fetch('/api/userlist')
             const data = await response.json()
             const users = data.responsedata
             const array = JSON.parse(users)
--- app/api/userpermonth/route.js
+++ app/api/userpermonth/route.js
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 export async function GET() {
     const today = new Date()
     const firstday = new Date(today.getFullYear(), today.getMonth(), 1)
