f2616980b7f4c145f840d62a150af138ac067260
lacrisun <gustipanji2006@gmail.com>
2023-12-03 18:36:16 +0700

    v1.0

--- app/api/accountcount/route.js
+++ app/api/accountcount/route.js
@@ -1,23 +1,8 @@
-/**
- * This module exports a function called `GET` that retrieves the count of user accounts and the count of verified user accounts from a database using the Prisma ORM. It then returns a JSON response with the counts and a status code.
- *
- * @module GET
- * @returns {Promise<object>} A JSON response object with the following structure:
- * - `responsedata`: A JSON string representing the count of user accounts.
- * - `isVerifiedData`: A JSON string representing the count of verified user accounts.
- * @throws {Error} If an error occurs during the execution of the `GET` function, it is caught and logged to the console. The code then returns a JSON response with an error message and a status code of 500.
- *
- * @example
- * const response = await GET();
- * console.log(response);
- * // Output: { responsedata: '...', isVerifiedData: '...' }
- */
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
-
 export async function GET() {
     try {
         const usercount = await prisma.accounts.count()
